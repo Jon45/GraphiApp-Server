18b680d3e9bed32e90b4cf8df8691b6311664a2b
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-10-22 23:08:36 +0200

    Quitado comentario erróneo

--- src/bl/LogicREST.java
+++ src/bl/LogicREST.java
@@ -1,24 +1,3 @@
-/*
- * $Id: businessREST.java Oct 22, 2015 tta1718$
- * 
- * Copyright (C) 2015 Maider Huarte Arrayago
- * 
- * This file is part of TTA1718_LS-EX_09S.zip.
- * 
- * TTA1718_LS-EX_09S.zip is based on templates by Eclipse.org and it is
- * intended for learning purpouses only.
- * 
- * TTA1718_LS-EX_09S-10.zip is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- * 
- * TTA1718_LS-EX_09S-10.zip is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details <http://www.gnu.org/licenses/>.
- */
-
 package bl;
 
 import java.util.ArrayList;
