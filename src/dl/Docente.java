c2150a8556e98b27195bc6c18f233e37d61b38c0
Estrella Basurto <ebasurto002@ehu.es>
2017-11-03 23:33:45 +0100

    Añadida Query para comprobar login y contraseña de Docente. Necesario para una modificación de loginUser en LogicREST.java

--- src/dl/Docente.java
+++ src/dl/Docente.java
@@ -11,7 +11,8 @@ import javax.persistence.*;
 @Entity
 @NamedQueries({
 @NamedQuery(name="Docente.findAll", query="SELECT d FROM Docente d"),
-@NamedQuery(name="Docente.findNickname", query="SELECT d FROM Docente d WHERE d.nickname = :nickname")})
+@NamedQuery(name="Docente.findNickname", query="SELECT d FROM Docente d WHERE d.nickname = :nickname"),
+@NamedQuery(name="Docente.findByNicknamePass", query="SELECT d FROM Docente d WHERE d.nickname= :nickname AND d.password= :password")})
 public class Docente implements Serializable {
 	private static final long serialVersionUID = 1L;
 
